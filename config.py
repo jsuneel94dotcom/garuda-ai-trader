668a7f13787fa8a7dca565cd388baa1936473d1c
jsuneel94dotcom <jsuneel94@gmail.com>
2026-07-14 13:27:34 +0530

    Create config.py
    import os
    
    TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
    
    NSE_ANNOUNCEMENT_URL = "https://www.nseindia.com/api/corporate-announcements"

---
+++ config.py
@@ -0,0 +1,6 @@
+import os
+
+TELEGRAM_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+NSE_ANNOUNCEMENT_URL = "https://www.nseindia.com/api/corporate-announcements"
