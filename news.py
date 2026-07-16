4dc0bc73d745686112d9bc2dba8371bca7f2cc3d
jsuneel94dotcom <jsuneel94@gmail.com>
2026-07-17 01:53:50 +0530

    Create news.py
    def get_positive_news():
        return [
            {
                "stock": "Demo Stock",
                "news": "Demo positive NSE announcement.",
                "impact": "HIGH"
            }
        ]

---
+++ news.py
@@ -0,0 +1,8 @@
+def get_positive_news():
+    return [
+        {
+            "stock": "Demo Stock",
+            "news": "Demo positive NSE announcement.",
+            "impact": "HIGH"
+        }
+    ]
