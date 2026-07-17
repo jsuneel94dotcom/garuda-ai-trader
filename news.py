cb544ab11e51b924fa2b010906b95664254ee1e7
jsuneel94dotcom <jsuneel94@gmail.com>
2026-07-18 03:10:32 +0530

    Update news.py
    def get_positive_news():
    
        return [
            {
                "stock": "BHEL",
                "news": "Received Rs. 500 Crore order.",
                "impact": "HIGH"
            },
            {
                "stock": "IRCON",
                "news": "Won new railway contract.",
                "impact": "HIGH"
            },
            {
                "stock": "TATA POWER",
                "news": "Announced capacity expansion.",
                "impact": "HIGH"
            }
        ]

--- news.py
+++ news.py
@@ -1,8 +1,19 @@
 def get_positive_news():
+
     return [
         {
-            "stock": "Demo Stock",
-            "news": "Demo positive NSE announcement.",
+            "stock": "BHEL",
+            "news": "Received Rs. 500 Crore order.",
+            "impact": "HIGH"
+        },
+        {
+            "stock": "IRCON",
+            "news": "Won new railway contract.",
+            "impact": "HIGH"
+        },
+        {
+            "stock": "TATA POWER",
+            "news": "Announced capacity expansion.",
             "impact": "HIGH"
         }
     ]
